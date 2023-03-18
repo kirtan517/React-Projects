81e6e0781a562082245e101be3c26fe485364e97
kanani kirtan <69329422+kirtan517@users.noreply.github.com>
2023-03-17 22:10:50 -0400

    Hide
    Hide the key

--- Project-5/client/src/components/Auth/GoogleAuthProvider.js
+++ Project-5/client/src/components/Auth/GoogleAuthProvider.js
@@ -3,8 +3,8 @@ export const GoogleAuthContext = createContext();
 
 function GoogleAuthProvider(props) {
 	const clientId =
-		"436866084784-qio70eakhv0sov1e35he60gg5g5tuun6.apps.googleusercontent.com";
-	const clientSecret = "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";
+		"ClientID";
+	const clientSecret = "CLientSecret";
 
 	const contextValue = useMemo(
 		() => ({ clientId: clientId, clientSecret: clientSecret }),
